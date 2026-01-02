1f9dd6f86c7dd120191aab1cc39538fa71037b33
Natalia-Schwindt <naty8014@gmail.com>
2026-01-02 19:29:32 -0300

    first commit

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,34 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListaMedico;
+import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @Transactional
+    @PostMapping
+    public void registrar(@RequestBody @Valid DatosRegistroMedico datos) {
+        repository.save(new Medico(datos));
+    }
+
+    @GetMapping
+    public Page<DatosListaMedico> listar(@PageableDefault(size=10, sort={"nombre"}) Pageable paginacion){
+        return repository.findAll(paginacion).map(DatosListaMedico::new);
+    }
+}
