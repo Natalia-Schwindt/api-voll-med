3640c81628cb101429e7a60f3a298d0e28603400
Natalia-Schwindt <naty8014@gmail.com>
2026-01-03 09:25:30 -0300

    feat: implement complete CRUD and logical delete for patients

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -21,16 +21,34 @@ public class Paciente {
     private String email;
     private String documentoIdentidad;
     private String telefono;
+    private Boolean activo;
 
     @Embedded
     private Direccion direccion;
 
     public Paciente(DatosRegistroPaciente datos) {
         this.id = null;
+        this.activo = true;
         this.nombre = datos.nombre();
         this.email = datos.email();
         this.telefono = datos.telefono();
         this.documentoIdentidad = datos.documento_identidad();
         this.direccion = new Direccion(datos.direccion());
+    }
+
+    public void actualizarInformaciones(DatosActualizacionPaciente datos) {
+        if (datos.nombre() != null) {
+            this.nombre = datos.nombre();
+        }
+        if (datos.telefono() != null) {
+            this.telefono = datos.telefono();
+        }
+        if (datos.direccion() != null) {
+            this.direccion.actualizarDireccion(datos.direccion());
+        }
+    }
+
+    public void eliminar() {
+        this.activo = false;
     }
 }
