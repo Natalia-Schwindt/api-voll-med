214ddc04e23d1d453821fdb14526f028d15734a6
Natalia-Schwindt <naty8014@gmail.com>
2026-01-07 19:54:17 -0300

    feat: implement detail endpoint and ResponseEntity for doctors and patients

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,5 +40,12 @@ public class PacienteController {
     public void eliminar(@PathVariable Long id) {
         var paciente = repository.getReferenceById(id);
         paciente.eliminar();
+    }
+
+    @GetMapping("/{id}")
+    @Transactional
+    public ResponseEntity detallar(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DatosDetallePaciente(paciente));
     }
 }
---
+++ src/main/java/med/voll/api/paciente/DatosDetallePaciente.java
@@ -0,0 +1,23 @@
+package med.voll.api.paciente;
+
+import med.voll.api.direccion.Direccion;
+
+public record DatosDetallePaciente(
+        Long id,
+        String nombre,
+        String email,
+        String telefono,
+        String documentoIdentidad,
+        Direccion direccion
+) {
+    public DatosDetallePaciente(Paciente paciente) {
+        this(
+                paciente.getId(),
+                paciente.getNombre(),
+                paciente.getEmail(),
+                paciente.getTelefono(),
+                paciente.getDocumentoIdentidad(),
+                paciente.getDireccion()
+        );
+    }
+}
