ae365af1687be17588208f0df4382bfadf8de961
Natalia-Schwindt <naty8014@gmail.com>
2026-01-18 21:45:57 -0300

    feat: complete consultation booking and cancellation flows

---
+++ src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -0,0 +1,67 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ReservaDeConsultas {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public void reservar(DatosReservaConsulta datos) {
+
+        if(!pacienteRepository.existsById(datos.idPaciente())) {
+            throw new ValidacionException("No existe un paciente con el id informado");
+        }
+
+        if(datos.idMedico() != null && !medicoRepository.existsById(datos.idMedico())) {
+            throw new ValidacionException("No existe un médico con el id informado");
+        }
+
+        var medico = elegirMedico(datos);
+        var paciente = pacienteRepository.findById(datos.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, datos.fecha(), null);
+
+        consultaRepository.save(consulta);
+    }
+
+    private Medico elegirMedico(DatosReservaConsulta datos) {
+        if(datos.idMedico() != null) {
+            return medicoRepository.getReferenceById(datos.idMedico());
+        }
+        if(datos.especialidad() == null) {
+            throw new ValidacionException("Es necesario elegir una especialidad cuando no se elige un médico");
+        }
+        return medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(datos.especialidad(), datos.fecha());
+    }
+
+    public void cancelar(DatosCancelamientoConsulta datos) {
+        if (!consultaRepository.existsById(datos.idConsulta())) {
+            throw new ValidacionException("Id de la consulta informado no existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(datos.idConsulta());
+
+        var ahora = LocalDateTime.now();
+        var diferenciaEnHoras = java.time.Duration.between(ahora, consulta.getFecha()).toHours();
+
+        if (diferenciaEnHoras < 24) {
+            throw new ValidacionException("¡Consulta solo puede ser cancelada con antecedencia mínima de 24 horas!");
+        }
+
+        consulta.cancelar(datos.motivo());
+    }
+}
